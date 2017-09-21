aed3a1764b610f45c227fac01cd9e1e174b99b8c
Diego Diaz <berith.py@gmail.com>
2017-09-20 22:55:38 -0400

    Some improvements! Reseting.

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # turtlesim-challenge
-Second part of an interview.
+Python and javascripts turtlesim examples.
+
+Before testing the scripts
+* Run `roscore`
+* Run `rosrun turtlesim turtelsim_node`
+
+Test the python script just execute it from the terminal.
+* Run `python2.7 turtlesim.py`
+
+Test the javascript by also launching rosbridge besides roscore and the turtlesim node.
+* Run `roslaunch rosbridge_server rosbridge_websocket.launch`
+* Open turtle-sim.html and press the draw pentagram button.
+
+The python script draws two stars, one after the other with 4 seconds between them.
+The javascript one draws a pentagram when you execute it.
+
+## Known bugs
+The javascript star drawer sometimes fails for no apparent reason.
+
+## TODO
+ * Improve javascript acurracy.
+ * Picture interpreter.
--- turtlesim.py
+++ turtlesim.py
@@ -2,13 +2,23 @@ import math
 from time import sleep
 
 import rospy
+import std_srvs.srv
 from geometry_msgs.msg import Twist
+
+reset = rospy.ServiceProxy('reset', std_srvs.srv.Empty())
+
 
 class Turtle(object):
     rospy.init_node('robot_cleaner', anonymous=True)
+
     velocity_publisher = rospy.Publisher('/turtle1/cmd_vel', Twist, queue_size=25)
     vel_msg = Twist()
-    def move(self,distance):
+
+    def move(self, distance):
+        """
+        Moves the turtle in a straight line.
+        :param distance: unit of distance traveled
+        """
         speed = 1
 
         self.vel_msg.linear.x = abs(speed)
@@ -28,8 +38,13 @@ class Turtle(object):
         self.stop_moving()
 
     def rotate(self, angle):
+        """
+        Rotate the turtle.
+        :param angle: degrees to rotate, positive or negative to move either left or right
+        :return: 
+        """
         angular_speed = 1.0
-        relative_angle = math.radians(angle)
+        relative_angle = math.radians(abs(angle))
 
         self.vel_msg.linear.x = 0
         self.vel_msg.linear.y = 0
@@ -37,7 +52,10 @@ class Turtle(object):
         self.vel_msg.angular.x = 0
         self.vel_msg.angular.y = 0
 
-        self.vel_msg.angular.z = abs(angular_speed)
+        if (angle>0):
+            self.vel_msg.angular.z = angular_speed
+        else:
+            self.vel_msg.angular.z = -angular_speed
         t0 = rospy.Time.now().to_sec()
         current_angle = 0
 
@@ -46,10 +64,9 @@ class Turtle(object):
             t1 = rospy.Time.now().to_sec()
             current_angle = angular_speed * (t1 - t0)
 
-        #detener a la tortuga
         self.stop_moving()
 
-    def forward(self,distancia):
+    def forward(self, distancia):
         self.move(distancia)
 
     def stop_moving(self):
@@ -61,30 +78,34 @@ class Turtle(object):
         self.vel_msg.angular.z = 0
         self.velocity_publisher.publish(self.vel_msg)
 
-def draw_pentagram(sides,size=1.8):
-    tortu = Turtle()
-    tortu.forward(0)
-    for side in range(sides):
-        sleep(0.5)
-        tortu.forward(size)
-        tortu.rotate(180)
-        tortu.rotate(360/(sides*2))
+    def __init__(self):
+        pass
 
-def draw_cute_star(size=1.8):
-    tortu = Turtle()
-    tortu.forward(0)
+
+def draw_pentagram(tortu, size=1.8):
+    for side in range(5):
+        tortu.forward(size)
+        tortu.rotate(-144)
+
+
+def draw_cute_star(tortu, size=1.8):
     for side in range(5):
         tortu.forward(size)
         tortu.rotate(36)
         tortu.forward(size)
-        tortu.rotate(252)
+        tortu.rotate(-108)
+
 
 if __name__ == '__main__':
     try:
-        #draw_pentagram(6)
-        draw_cute_star(1.5)
+        turtle = Turtle()
+        reset()
+        draw_cute_star(turtle, 1.5)
+        print("Drawing cute star!")
+        sleep(4)
+        reset()
+        draw_pentagram(turtle,1.8)
+        print("Drawing pentagram.")
 
-            # except rospy.ROSInterruptException:
-        #     pass
-    except:
+    except rospy.ROSInterruptException:
         pass
